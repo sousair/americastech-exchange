72c33fb3bf11acd92d6c49d6ca6697be90f67842
sousair <sousair2001@gmail.com>
2023-12-14 06:37:05 -0300

    feat: init echo http server and create order route

--- cmd/httpserver/main.go
+++ cmd/httpserver/main.go
@@ -1,7 +1,60 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"log"
+	"os"
+
+	"github.com/joho/godotenv"
+	"github.com/labstack/echo/v4"
+	"gorm.io/driver/postgres"
+	"gorm.io/gorm"
+
+	app_usecases "github.com/sousair/americastech-exchange/internal/application/usecases"
+	gorm_models "github.com/sousair/americastech-exchange/internal/infra/database/models"
+	gorm_repositories "github.com/sousair/americastech-exchange/internal/infra/database/repositories"
+	binance_exchange "github.com/sousair/americastech-exchange/internal/infra/providers/exchange"
+	http_handlers "github.com/sousair/americastech-exchange/internal/presentation/http/handlers"
+)
 
 func main() {
-	fmt.Println("Hello World!")
+	err := godotenv.Load()
+
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	var (
+		binanceApiKey     = os.Getenv("BINANCE_API_KEY")
+		binanceSecret     = os.Getenv("BINANCE_API_SECRET")
+		binanceApiBaseUrl = os.Getenv("BINANCE_API_BASE_URL")
+
+		postgresConnectionUrl = os.Getenv("POSTGRES_CONNECTION_URL")
+	)
+
+	fmt.Println("Binance API Key: ", binanceApiKey)
+	fmt.Println("Binance API Secret: ", binanceSecret)
+	fmt.Println("Binance Base URL: ", binanceApiBaseUrl)
+	fmt.Println("Postgres Connection URL: ", postgresConnectionUrl)
+
+	db, err := gorm.Open(postgres.Open(postgresConnectionUrl), &gorm.Config{})
+
+	if err != nil {
+		panic(err)
+	}
+
+	db.AutoMigrate(&gorm_models.Order{})
+
+	orderRepository := gorm_repositories.NewOrderRepository(db)
+	binanceExchangeProvider := binance_exchange.NewBinanceExchangeProvider(binanceApiKey, binanceSecret, binanceApiBaseUrl)
+
+	createUserUC := app_usecases.NewCreateOrderUseCase(orderRepository, binanceExchangeProvider)
+
+	createOrderHandler := http_handlers.NewCreateOrderHandler(createUserUC).Handle
+
+	e := echo.New()
+
+	e.POST("/orders", createOrderHandler)
+
+	e.Logger.Fatal(e.Start(":7070"))
 }
--- go.mod
+++ go.mod
@@ -5,13 +5,18 @@ go 1.21.3
 require (
 	github.com/adshao/go-binance/v2 v2.4.5
 	github.com/google/uuid v1.5.0
+	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo/v4 v4.11.3
+	gorm.io/driver/postgres v1.5.4
 	gorm.io/gorm v1.25.5
 )
 
 require (
 	github.com/bitly/go-simplejson v0.5.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/jackc/pgpassfile v1.0.0 // indirect
+	github.com/jackc/pgservicefile v0.0.0-20221227161230-091c0ba34f0a // indirect
+	github.com/jackc/pgx/v5 v5.4.3 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
@@ -21,6 +26,7 @@ require (
 	github.com/mattn/go-isatty v0.0.19 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180228061459-e0a39a4cb421 // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/rogpeppe/go-internal v1.12.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/crypto v0.14.0 // indirect
