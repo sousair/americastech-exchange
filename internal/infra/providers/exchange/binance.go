87a53800142a626525cfc6b344495b2f6cae2ae6
sousair <sousair2001@gmail.com>
2023-12-14 05:40:56 -0300

    feat: add binance exchange provider

---
+++ internal/infra/providers/exchange/binance.go
@@ -0,0 +1,90 @@
+package binance_exchange
+
+import (
+	"context"
+	"fmt"
+
+	"github.com/adshao/go-binance/v2"
+	"github.com/sousair/americastech-exchange/internal/application/providers/exchange"
+	"github.com/sousair/americastech-exchange/internal/core/enums"
+)
+
+type (
+	BinanceExchangeProvider struct {
+		client *binance.Client
+	}
+)
+
+func NewBinanceExchangeProvider(apiKey, secret, baseUrl string) exchange.ExchangeProvider {
+	binanceClient := binance.NewClient(apiKey, secret)
+	binanceClient.BaseURL = baseUrl
+	binanceClient.Debug = true
+
+	return &BinanceExchangeProvider{
+		client: binanceClient,
+	}
+}
+
+func (b BinanceExchangeProvider) Create(params exchange.CreateOrderParams) (*exchange.CreatedOrder, error) {
+	var binanceOrderService *binance.CreateOrderService
+
+	switch params.Type {
+	case "MARKET":
+		binanceOrderService = b.mountMarketOrder(params.Pair, string(params.Direction), params.Amount)
+	case "LIMIT":
+		binanceOrderService = b.mountLimitOrder(params.Pair, string(params.Direction), params.Amount, params.Price)
+	}
+
+	binanceOrderRes, err := binanceOrderService.Do(context.Background())
+
+	if err != nil {
+		return nil, err
+	}
+
+	createdOrder := &exchange.CreatedOrder{
+		ExternalID: fmt.Sprint(binanceOrderRes.OrderID),
+		Pair:       binanceOrderRes.Symbol,
+		Direction:  enums.OrderDirection(binanceOrderRes.Side),
+		Type:       enums.OrderType(binanceOrderRes.Type),
+		Amount:     binanceOrderRes.OrigQuantity,
+		Price:      binanceOrderRes.Price,
+		Status:     parseBinanceStatus(binanceOrderRes.Status),
+	}
+
+	return createdOrder, nil
+}
+
+func (b BinanceExchangeProvider) mountMarketOrder(pair, direction, quantity string) *binance.CreateOrderService {
+	return b.client.
+		NewCreateOrderService().
+		Type("MARKET").
+		Symbol(pair).
+		Side(binance.SideType(direction)).
+		Quantity(quantity)
+}
+
+func (b BinanceExchangeProvider) mountLimitOrder(pair, direction, quantity, price string) *binance.CreateOrderService {
+	return b.client.
+		NewCreateOrderService().
+		Type("LIMIT").
+		Symbol(pair).
+		Side(binance.SideType(direction)).
+		Quantity(quantity).
+		TimeInForce(binance.TimeInForceTypeGTC).
+		Price(price)
+}
+
+func parseBinanceStatus(status binance.OrderStatusType) enums.OrderStatus {
+	switch status {
+	case binance.OrderStatusTypeNew:
+		return enums.Open
+	case binance.OrderStatusTypePartiallyFilled:
+		return enums.PartiallyFilled
+	case binance.OrderStatusTypeFilled:
+		return enums.Filled
+	case binance.OrderStatusTypeCanceled:
+		return enums.Canceled
+	default:
+		return ""
+	}
+}
